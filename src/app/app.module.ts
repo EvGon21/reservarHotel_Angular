a92d0edd017ba2b880c304cfa2da410c031ef04d
Tony <pp.tony777@gmail.com>
2023-04-19 11:53:30 -0600

    Implementacion de todos los componentes del nav-bar y header, body y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/nav-bar/about/about.component';
+import { ContactComponent } from './components/nav-bar/contact/contact.component';
+import { RoomTypesComponent } from './components/nav-bar/room-types/room-types.component';
+import { FeedbackComponent } from './components/nav-bar/feedback/feedback.component';
+import { HeaderComponentComponent } from './components/header/header-component/header-component.component';
+import { BodyComponentComponent } from './components/body/body-component/body-component.component';
+import { FooterComponentComponent } from './components/footer/footer-component/footer-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    RoomTypesComponent,
+    FeedbackComponent,
+    HeaderComponentComponent,
+    BodyComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/nav-bar/room-types/room-types.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-types',
+  templateUrl: './room-types.component.html',
+  styleUrls: ['./room-types.component.css']
+})
+export class RoomTypesComponent {
+
+}
