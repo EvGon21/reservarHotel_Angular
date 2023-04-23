d915f986bf11ba8f7a2140e5da143815baf8af6f
Tony <pp.tony777@gmail.com>
2023-04-22 19:00:28 -0600

    Intento de implementacion de masory (todavia no funciona)

--- src/app/components/nav-bar/room-types/room-types.component.ts
+++ src/app/components/nav-bar/room-types/room-types.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import * as Masonry from 'masonry-layout';
 
 @Component({
   selector: 'app-room-types',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./room-types.component.css']
 })
 export class RoomTypesComponent {
-
+  images = [
+    { url: '../../../../assets/imgs/rooms/room1.jpg' },
+    { url: '../../../../assets/imgs/rooms/room2.jpg' },
+    { url: '../../../../assets/imgs/rooms/room3.jpg' },
+    { url: '../../../../assets/imgs/rooms/room4.jpg' },
+    { url: '../../../../assets/imgs/rooms/room5.jpg' },
+    { url: '../../../../assets/imgs/rooms/room6.jpg' },
+    { url: '../../../../assets/imgs/rooms/room7.jpg' },
+    { url: '../../../../assets/imgs/rooms/room8.jpg' },
+    { url: '../../../../assets/imgs/rooms/room3.jpg' }
+  ];
+  
+  ngAfterViewInit() {
+    var grid = document.querySelector('.grid');
+    if (grid) {
+      var msnry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+      });
+    }
+  }
+  
 }
