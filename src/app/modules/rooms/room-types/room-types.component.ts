f227935c6667767f4ae6cfb58aa67eaac77296ca
Tony <pp.tony777@gmail.com>
2023-04-25 14:44:19 -0600

    Nuevas fotos de rooms, implementacion de estilo masorny con CSS

--- src/app/modules/rooms/room-types/room-types.component.ts
+++ src/app/modules/rooms/room-types/room-types.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./room-types.component.css']
 })
 export class RoomTypesComponent {
-
+  images = [
+    { url: '../../../../assets/imgs/rooms/room1.jpg'},
+    { url: '../../../../assets/imgs/rooms/room2.jpg'},
+    { url: '../../../../assets/imgs/rooms/room3.jpg'},
+    { url: '../../../../assets/imgs/rooms/room4.jpg'},
+    { url: '../../../../assets/imgs/rooms/room5.jpg'},
+    { url: '../../../../assets/imgs/rooms/room6.jpg'},
+    { url: '../../../../assets/imgs/rooms/room7.jpg'},
+    { url: '../../../../assets/imgs/rooms/room8.jpg'},
+    { url: '../../../../assets/imgs/rooms/room9.jpg'},
+    { url: '../../../../assets/imgs/rooms/room10.jpg'}
+  ];
 }
